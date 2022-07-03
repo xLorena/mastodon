25d160007f670c219908f08ee1155cdca1c656ae
xLorena <lb@bitsandlikes.de>
2022-07-03 17:59:39 +0200

    added possibility to hide labels of navbar (not used)

--- app/javascript/mastodon/features/ui/components/navigation_panel.js
+++ app/javascript/mastodon/features/ui/components/navigation_panel.js
@@ -1,15 +1,17 @@
 /* eslint-disable jsx-quotes */
-import React from "react";
-import { NavLink, withRouter } from "react-router-dom";
-import { FormattedMessage } from "react-intl";
-import Icon from "mastodon/components/icon";
-import { profile_directory, showTrends } from "mastodon/initial_state";
-import NotificationsCounterIcon from "./notifications_counter_icon";
-import FollowRequestsNavLink from "./follow_requests_nav_link";
-import ListPanel from "./list_panel";
-import TrendsContainer from "mastodon/features/getting_started/containers/trends_container";
+import React from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
+import Icon from 'mastodon/components/icon';
+import { profile_directory, showTrends } from 'mastodon/initial_state';
+//import NotificationsCounterIcon from './notifications_counter_icon';
+import FollowRequestsNavLink from './follow_requests_nav_link';
+import ListPanel from './list_panel';
+import TrendsContainer from 'mastodon/features/getting_started/containers/trends_container';
+import PropTypes from 'prop-types';
 
-const NavigationPanel = () => (
+
+const NavigationPanel = (props) => (
   <div className="navigation-panel">
     <NavLink
       className="column-link column-link--transparent"
@@ -18,7 +20,9 @@ const NavigationPanel = () => (
       data-preview-icon="home"
     >
       <Icon className="column-link__icon" id="home" fixedWidth />
-      <FormattedMessage id="tabs_bar.home" defaultMessage="Home" />
+      <span className={props.showLabelOnHover ? 'show-on-hover' : null}>
+        <FormattedMessage  id="tabs_bar.home" defaultMessage="Home" />
+      </span>
     </NavLink>
     {/* <NavLink
       className="column-link column-link--transparent"
@@ -40,7 +44,9 @@ const NavigationPanel = () => (
       data-preview-icon="users"
     >
       <Icon className="column-link__icon" id="users" fixedWidth />
-      <FormattedMessage id="tabs_bar.local_timeline" defaultMessage="Local" />
+      <span className={props.showLabelOnHover ? 'show-on-hover' : null}>
+        <FormattedMessage id="tabs_bar.local_timeline" defaultMessage="Local" />
+      </span>
     </NavLink>
     <NavLink
       className="column-link column-link--transparent"
@@ -50,10 +56,12 @@ const NavigationPanel = () => (
       data-preview-icon="globe"
     >
       <Icon className="column-link__icon" id="globe" fixedWidth />
-      <FormattedMessage
-        id="tabs_bar.federated_timeline"
-        defaultMessage="Federated"
-      />
+      <span className={props.showLabelOnHover ? 'show-on-hover' : null}>
+        <FormattedMessage
+          id="tabs_bar.federated_timeline"
+          defaultMessage="Federated"
+        />
+      </span>
     </NavLink>
     {/* <NavLink
       className="column-link column-link--transparent"
@@ -83,7 +91,8 @@ const NavigationPanel = () => (
       <Icon className="column-link__icon" id="list-ul" fixedWidth />
       <FormattedMessage id="navigation_bar.lists" defaultMessage="Lists" />
     </NavLink> */}
-    <NavLink
+    {/* ________________________________________________________________ */}
+    {/* <NavLink
       className="column-link column-link--transparent"
       to="/newsfeed-compare"
     >
@@ -109,14 +118,16 @@ const NavigationPanel = () => (
         id="navigation_bar.awareness"
         defaultMessage="Awareness"
       />
-    </NavLink>
+    </NavLink>*/}
     {profile_directory && (
       <NavLink className="column-link column-link--transparent" to="/directory">
         <Icon className="column-link__icon" id="address-book-o" fixedWidth />
-        <FormattedMessage
-          id="getting_started.directory"
-          defaultMessage="Profile directory"
-        />
+        <span className={props.showLabelOnHover ? 'show-on-hover' : null}>
+          <FormattedMessage
+            id="getting_started.directory"
+            defaultMessage="Profile directory"
+          />
+        </span>
       </NavLink>
     )}
 
@@ -129,22 +140,66 @@ const NavigationPanel = () => (
       href="/settings/preferences"
     >
       <Icon className="column-link__icon" id="cog" fixedWidth />
-      <FormattedMessage
-        id="navigation_bar.preferences"
-        defaultMessage="Preferences"
-      />
+      <span className={props.showLabelOnHover ? 'show-on-hover' : null}>
+        <FormattedMessage
+          id="navigation_bar.preferences"
+          defaultMessage="Preferences"
+        />
+      </span>
     </a>
     <a className="column-link column-link--transparent" href="/relationships">
       <Icon className="column-link__icon" id="users" fixedWidth />
-      <FormattedMessage
-        id="navigation_bar.follows_and_followers"
-        defaultMessage="Follows and followers"
-      />
+      <span className={props.showLabelOnHover ? 'show-on-hover' : null}>
+        <FormattedMessage
+          id="navigation_bar.follows_and_followers"
+          defaultMessage="Follows and followers"
+        />
+      </span>
     </a>
+
+    <hr />
+
+    <NavLink
+      className="column-link column-link--transparent"
+      to="/newsfeed-compare"
+    >
+      <Icon className="column-link__icon" id="columns" fixedWidth />
+      <span className={props.showLabelOnHover ? 'show-on-hover' : null}>
+        <FormattedMessage
+          id="navigation_bar.newsfeed-vergleich"
+          defaultMessage="Newsfeed Vergleich"
+        />
+      </span>
+    </NavLink>
+    <NavLink
+      className="column-link column-link--transparent"
+      to="/polarization"
+    >
+      <Icon className="column-link__icon" id="user-circle-o" fixedWidth />
+      <span className={props.showLabelOnHover ? 'show-on-hover' : null}>
+        <FormattedMessage
+          id="navigation_bar.polarisierung"
+          defaultMessage="Polarisierung erkunden"
+        />
+      </span>
+    </NavLink>
+    <NavLink className="column-link column-link--transparent" to="/awareness">
+      <Icon className="column-link__icon" id="lightbulb-o" fixedWidth />
+      <span className={props.showLabelOnHover ? 'show-on-hover' : null}>
+        <FormattedMessage
+          id="navigation_bar.awareness"
+          defaultMessage="Awareness"
+        />
+      </span>
+    </NavLink>
 
     {showTrends && <div className="flex-spacer" />}
     {showTrends && <TrendsContainer />}
   </div>
 );
+
+NavigationPanel.propTypes = {
+  showLabelOnHover: PropTypes.bool,
+};
 
 export default withRouter(NavigationPanel);
