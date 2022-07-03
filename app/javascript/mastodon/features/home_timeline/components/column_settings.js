a72058cbae9ec6ec7e179abc1907439026686117
xLorena <lb@bitsandlikes.de>
2022-07-03 17:49:30 +0200

    added RadioButtons to Home Column_settings

--- app/javascript/mastodon/features/home_timeline/components/column_settings.js
+++ app/javascript/mastodon/features/home_timeline/components/column_settings.js
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { injectIntl, FormattedMessage } from 'react-intl';
 import SettingToggle from '../../notifications/components/setting_toggle';
+import SettingRadio from '../../notifications/components/setting_radio';
+import DropdownMenuContainer from '../../../containers/dropdown_menu_container';
 
-export default @injectIntl
+export default
+@injectIntl
 class ColumnSettings extends React.PureComponent {
 
   static propTypes = {
@@ -13,19 +16,121 @@ class ColumnSettings extends React.PureComponent {
     intl: PropTypes.object.isRequired,
   };
 
-  render () {
+
+
+  render() {
     const { settings, onChange } = this.props;
+    const menu = [];
+    menu.push(  <div style={{ width: 100, height: 100, backgroundColor: 'green' }} />);
 
     return (
       <div>
-        <span className='column-settings__section'><FormattedMessage id='home.column_settings.basic' defaultMessage='Basic' /></span>
+        <span className='column-settings__section'>
+          <FormattedMessage
+            id='home.column_settings.basic'
+            defaultMessage='Basic'
+          />
+        </span>
 
         <div className='column-settings__row'>
-          <SettingToggle prefix='home_timeline' settings={settings} settingPath={['shows', 'reblog']} onChange={onChange} label={<FormattedMessage id='home.column_settings.show_reblogs' defaultMessage='Show boosts' />} />
+          <SettingToggle
+            prefix='home_timeline'
+            settings={settings}
+            settingPath={['shows', 'reblog']}
+            onChange={onChange}
+            label={
+              <FormattedMessage
+                id='home.column_settings.show_reblogs'
+                defaultMessage='Show boosts'
+              />
+            }
+          />
         </div>
 
         <div className='column-settings__row'>
-          <SettingToggle prefix='home_timeline' settings={settings} settingPath={['shows', 'reply']} onChange={onChange} label={<FormattedMessage id='home.column_settings.show_replies' defaultMessage='Show replies' />} />
+          <SettingToggle
+            prefix='home_timeline'
+            settings={settings}
+            settingPath={['shows', 'reply']}
+            onChange={onChange}
+            label={
+              <FormattedMessage
+                id='home.column_settings.show_replies'
+                defaultMessage='Show replies'
+              />
+            }
+          />
+        </div>
+        <span className='column-settings__section'>
+          <FormattedMessage
+            id='home.column_settings.filteralgorithm'
+            defaultMessage='Filterungsalgorithmus'
+          />
+        </span>
+
+        <div className='column-settings__row'>
+          <SettingRadio
+            prefix='home_timeline'
+            // settings={settings}
+            // settingPath={['algorithm', 'default']}
+            // onChange={onChange}
+            value='default'
+            label={
+              <FormattedMessage
+                id='home.column_settings.default_algo'
+                defaultMessage='Keine/ Chronologisch'
+              />
+            }
+          />
+        </div>
+
+        <div className='column-settings__row'>
+          <SettingRadio
+            prefix='home_timeline'
+            // settings={settings}
+            // settingPath={['algorithm', 'diversity']}
+            // onChange={onChange}
+            value='diversity'
+            label={
+              <FormattedMessage
+                id='home.column_settings.diversity_algo'
+                defaultMessage='Divers'
+              />
+            }
+          />
+        </div>
+        <div className='column-settings__row'>
+          <SettingRadio
+            prefix='home_timeline'
+            // settings={settings}
+            // settingPath={['algorithm', 'newness']}
+            // onChange={onChange}
+            value='newness'
+            label={
+              <FormattedMessage
+                id='home.column_settings.newness_algo'
+                defaultMessage='Neuheitsbasiert'
+              />
+            }
+          />
+        </div>
+        <div className='column-settings__row'>
+          <SettingRadio
+            prefix='home_timeline'
+            // settings={settings}
+            // settingPath={['algorithm', 'user']}
+            value='user'
+            //onChange={onChange}
+            label={
+              <>
+                <FormattedMessage
+                  id='home.column_settings.user_algo'
+                  defaultMessage='Benutzerdefiniert'
+                />
+                <DropdownMenuContainer items={menu} icon='chevron-down' size={16} direction='right' />
+              </>
+            }
+          />
         </div>
       </div>
     );
