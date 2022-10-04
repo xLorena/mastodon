a676a56121b7068e0a8d368dcafde0822a9b90ed
xLorena <lb@bitsandlikes.de>
2022-10-04 20:31:25 +0200

    added mapping for Classification

---
+++ app/javascript/mastodon/utils/mappings.js
@@ -0,0 +1,13 @@
+export const CLASSIFICATION = {
+  'Optimistic': 0.0,
+  'Thankful': 1.0,
+  'Empathetic': 2.0,
+  'Pessimistic': 3.0,
+  'Anxious': 4.0,
+  'Sad':5.0,
+  'Annoyed':6.0,
+  'Denial':7.0,
+  'Surprise':8.0,
+  'Official report': 9.0,
+  'Joking': 10.0,
+};
