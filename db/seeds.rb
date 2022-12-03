8e4017a662e4f850dddcb08b88bc7e3e31c8ea40
xLorena <lb@bitsandlikes.de>
2022-12-03 02:49:07 +0100

    added comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,23 +13,31 @@ if Rails.env.development?
   admin.save(validate: false)
   adminId = admin.id
   User.where(email: "admin@#{domain}").first_or_initialize(email: "admin@#{domain}", password: 'mastodonadmin', password_confirmation: 'mastodonadmin', confirmed_at: Time.now.utc, admin: true, account: admin, agreement: true, approved: true).save!
+  
   #create testuser
-  du  = Account.where(username: 'du').first_or_initialize(username: 'du')
-  du.save(validate: false)
-  duId = du.id
+  you  = Account.where(username: 'du').first_or_initialize(username: 'du')
+  you.save(validate: false)
+  youId = you.id
   User.where(email: "du@#{domain}").first_or_initialize(email: "du@#{domain}", password: 'mastodondu', password_confirmation: 'mastodondu', confirmed_at: Time.now.utc, account: du, agreement: true, approved: true).save!
 
-
+  #read file in datasets/covid/test4.0.csv
   csv_text = File.read(Rails.root.join('datasets', 'covid', 'test4.0.csv'))
+  #parse file into object
   csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
   counter = 0
+  #loop through parsed object
   csv.each do |row|
-    puts row.to_hash
+    #create Account for each column, set username and avatar_remote_url, use counter for different names and pictures
     account  = Account.where(username: "user#{counter}").first_or_initialize(username: "user#{counter}", avatar_remote_url:"https://i.pravatar.cc/150?u=#{counter}")
+    #save to databse
     account.save(validate: false)
+    #create User for each column, set email, passwort, other variables and created account-variable as account and save to database
     User.where(email: "user#{counter}@#{domain}").first_or_initialize(email: "user#{counter}@#{domain}", password: 'mastodontest', password_confirmation: 'mastodontest', confirmed_at: Time.now.utc, account: account, agreement: true, approved: true).save!
-    Follow.create(account_id: duId, target_account_id: account.id, created_at: Time.now.utc + counter * 5, updated_at: Time.now.utc + counter * 5)
+    #create Follow for each column, with the testuser (you) following the newly created account and save to database, increase cration time a bit
+    Follow.create(account_id: youId, target_account_id: account.id, created_at: Time.now.utc + counter * 5, updated_at: Time.now.utc + counter * 5)
+    #create Status for each column, set the newly created account as account_id and set content of the row named Tweet as text, row Labels as sentiment_score and row Score as polarization_score, converting both to float, increase creation time a bit
     Status.create(account_id: account.id, text: row['Tweet'], created_at: Time.now.utc + counter * 10, updated_at: Time.now.utc + counter * 10, local: true, sentiment_score: row['Labels'].to_f, polarization_score: row['Score'].to_f )
+    #increase counter
     counter = counter + 1
   end
 
